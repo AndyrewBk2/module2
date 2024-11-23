ef9576aac884785512878fb31ccf28e2c83c4be4
Andrew Obrochta <andrew.obrochta@snhu.edu>
2024-11-23 12:46:03 -0700

    Update travel.js and travel.hbs, and add data folder

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,7 @@
+var fs = require('fs');
+var trips =
+JSON.parse(fs.readFileSync('.data/trips.json','utf8'));
+
 /* GET travel veiw */
 const travel = (req, res) => {
     res.render('travel', { title: 'Travlr Getaways'});
